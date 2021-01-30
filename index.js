5d7925b9c725eb2507d2399cd058ed5e2f8b6dda
fentyhall <fentymhall@gmail.com>
2021-01-30 13:07:12 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;
+const leastFavoriteCustomer = 'shelby';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+// If you try to use `return`, it will return 'BOB' but it will not re-define the variable
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'ian';
+}
+
+changeLeastFavoriteCustomer();
+// Uncaught SyntaxError: Identifier 'leastFavoriteCustomer' has already been declared
